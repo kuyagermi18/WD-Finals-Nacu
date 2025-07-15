a92ff9f73d3351af309a3a11ba0878690ae2e06e
kuyagermi18 <jamesnacu18@gmail.com>
2025-07-15 18:15:34 +0800

    refactor: updated landing page visuals

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+        window.addEventListener('DOMContentLoaded', () => {
+            const fadeEl = document.querySelector('.fade-in');
+            fadeEl.classList.add('loaded');
+        });
+
+        
